bda5053dc267dd3454cea7a08ecd30523a7f184e
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-10-20 13:54:31 +0200

    nav rotation logic

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
+let button_open = document.getElementById("open");
+let button_close = document.getElementById("close");
+let container = document.querySelector(".container");
 
+button_open.addEventListener("click", () => {
+  container.classList.add("show-nav");
+});
 
+button_close.addEventListener("click", () => {
+  container.classList.remove("show-nav");
+});
